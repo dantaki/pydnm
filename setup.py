e1f3f8736a7f49193f900c6eed04aeb41b95d6dd
dantaki <dantaki@ucsd.edu>
2018-06-01 10:50:22 +0000

    fixed float conversion error for PL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
 	name='pyDNM',
-	version='0.0.1',
+	version='0.0.2',
 	url='https://github.com/dantaki/pydnm',
 	author='Danny Antaki, Aojie Lian',
 	author_email='dantaki@ucsd.edu',
